b394b2c4aeb61ad2f36f5ff2f3c3633556c6ce8a
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-04-22 13:14:03 +0200

    servic static file with express.static

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
+//imports
 const cors = require("cors");
 const express = require("express");
-const app = express();
-const port = 8000;
+const fs = require("fs/promises");
 
-console.log("Le module CORS, c'est ça -> ", cors);
+const app = express();
+const port = 3000;
+
+// le serveur envoie la première vue statique
+app.use(express.static("frontend"));
 app.use(cors());
+console.log("Le module CORS, c'est ça -> ", cors);
+
+/*app.get("/", (req, res) => {
+  res.send("Welcome");
+});*/
 
 app.get("/api/drive", (req, res) => {
-  res.send("test");
+  res.send("Welcome");
+});
+
+app.post("/api/drive/:name", (req, res) => {
+  const foldername = req.params.name;
+  if (!foldername) {
+    res.end();
+  } else {
+    fs.mkdir(foldername)
+      .then(() => {
+        console.log("Y'a ça dans foldername -> ", foldername);
+        res.sendStatus(201);
+      })
+      .catch((error) => {
+        console.error("La création du dossier a échoué: ", error);
+        res.sendStatus(500);
+      });
+  }
 });
 
 app.listen(port, () => {
